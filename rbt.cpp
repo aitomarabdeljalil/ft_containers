86fee1ed60b0f821d8daca7abf40bd1f513b2eee
Abdeljalil Ait omar <aait-oma@e2r3p9.1337.ma>
2023-02-15 19:50:52 +0100

    insert fixed

--- rbt.cpp
+++ rbt.cpp
@@ -6,12 +6,25 @@
 /*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 18:20:56 by aait-oma          #+#    #+#             */
-/*   Updated: 2023/02/14 14:14:54 by aait-oma         ###   ########.fr       */
+/*   Updated: 2023/02/15 19:49:57 by aait-oma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <random>
+#include <ctime>
+#include <unistd.h>
+// for ckecker
+#include <cstddef>
+#include <map>
+#include <ostream>
+#include <queue>
+#include <iomanip>
+// end
 enum Color {RED, BLACK, DBLACK};
 enum Events {
 	def = 0,
@@ -20,7 +33,7 @@ enum Events {
 	RightRot,
 	RightRotColorSwap,
 };
-// template <typename T>
+
 struct Node
 {
 	int data;
@@ -54,14 +67,33 @@ void print2D(Node* root)
 	std::cout << "-------------------------\n";
 }
 
-// template <typename T>
+enum e_directions { LEFT, DEFAULT, RIGHT };
+struct output {
+	Node	*_nd;
+	int			 _lvl;
+	int			 _spaces;
+	int			 _addit;
+	int			 _length;
+	e_directions _dir;
+	static int	 total_lvls;
+	static int	 total_diff;
+	static int	 _width;
+	output( Node *nd, int lvl, int length, e_directions dir )
+		: _nd( nd ), _lvl( lvl ), _length( length ? length : 1 ), _dir( dir ), _addit( 0 ) {};
+};
+
+int output::total_lvls = 0;
+int output::total_diff = 2;
+int output::_width	 = 0;
+
 class RedBlackTree
 {
 public:
 	Node *root;
 	Node *nil;
 public:
-	RedBlackTree() :root(NULL) {
+	RedBlackTree() :root(NULL)
+	{
 		// nil = new Node(INT_MIN);
 		// nil->color = BLACK;
 		// nil->left = nil;
@@ -138,31 +170,7 @@ public:
 	{
 		if (!node) {
 			node = new Node(key, _parent);
-			if (_parent) {
-				if (node->parent->color == RED) {
-					Node* grandParent = node->parent->parent;
-					Node* uncle = getUncle(node);
-					if (grandParent && uncle && uncle->color == RED) {
-						node->parent->color = BLACK;
-						uncle->color = BLACK;
-						grandParent->color = RED;
-						print2D(root);
-					} 
-					else if (grandParent) {
-						if (node->data < node->parent->data && node->parent->data < grandParent->data) {
-							grandParent->event = RightRotColorSwap;
-						} else if (node->data > node->parent->data && node->parent->data > grandParent->data) {
-							grandParent->event = LeftRotColorSwap;
-						} else if (node->data > node->parent->data &&  node->parent->data < grandParent->data) {
-							node->parent->event = LeftRot;
-							grandParent->event = RightRotColorSwap;
-						} else {
-							node->parent->event = RightRot;
-							grandParent->event = LeftRotColorSwap;
-						}
-					}
-				}	
-			} 
+			return node;
 		}
 		else if (key > node->data)
 			node->right = rbInsertFixup(node->right, key, node);
@@ -172,6 +180,29 @@ public:
 			return node;
 		if (!node->parent)
 			node->color = BLACK;
+		else if ((node->left && node->left->color == RED) ||  (node->right && node->right->color == RED)) {
+			if (node->color == RED) {
+				Node* sib = findSib(node->parent, node);
+				if (sib && sib->color == RED) {
+					node->color = BLACK;
+					sib->color = BLACK;
+					node->parent->color = RED;
+				} 
+				else {
+					if (key < node->data && node->data < node->parent->data) {
+						node->parent->event = RightRotColorSwap;
+					} else if (key > node->data && node->data > node->parent->data) {
+						node->parent->event = LeftRotColorSwap;
+					} else if (key > node->data &&  node->data < node->parent->data) {
+						node->event = LeftRot;
+						node->parent->event = RightRotColorSwap;
+					} else {
+						node->event = RightRot;
+						node->parent->event = LeftRotColorSwap;
+					}
+				}
+			}	
+		}
 		if (node->event != def) {
 			Events ev = node->event;
 			node->event = def;
@@ -184,13 +215,25 @@ public:
 				node = LRotation(node);
 				if (ev == LeftRotColorSwap)
 					colorSwap(node, node->left);
+
 			}
 		}
+			
 		return node;
 	}
 	void	insert(Node *node, int key)
 	{
+		// std::cout << " INSERTING " << std::setw( 16 ) << key << "\n";
 		root = rbInsertFixup(node, key);
+		// log();
+		// checking
+		std::map< Node *, std::vector< int > > mp;
+		std::cout << "\e[1;31m";
+		assert( check( root, mp ) );
+		std::cout << "\e[0m";
+		std::cout << " INSERTED " << std::setw( 16 ) << key
+				  << " SUCCESSFULLY\n\e[F\e[K";
+				//   << " SUCCESSFULLY\n";
 	}
 	Node*	SearchNode(Node* node, int key)
 	{
@@ -209,7 +252,7 @@ public:
 	}
 	Node*	findSib(Node* parent, Node* child)
 	{
-		if (parent->left->data == child->data)
+		if (parent && parent->left && parent->left->data == child->data)
 			return parent->right;
 		return parent->left;
 	}
@@ -223,16 +266,17 @@ public:
 		}
 		Node* sib = findSib(node->parent, node);
 		Node* grandparent = sib->parent->parent;
+		Node* nparent = node->parent;
 		if (sib && sib->color == RED) {	
 			if (node->parent->left == node)
-				tmp = LRotation(node->parent);
+				nparent = LRotation(node->parent);
 			else
-				tmp = RRotation(node->parent);
+				nparent = RRotation(node->parent);
 			colorSwap(node->parent, sib);
-			print2D(root);
+			// print2D(root);
 			fixDB(node);
-			std::cout << "i am " << node->data << ", my parent is " << node->parent->data<< ", my grandparent is " << node->parent->parent->data << '\n';
-			print2D(root);
+			// std::cout << "i am " << node->data << ", my parent is " << node->parent->data<< ", my grandparent is " << node->parent->parent->data << '\n';
+			// print2D(root);
 		} else {
 			if (!sib || (sib && (!sib->right && !sib->left)) 
 				|| (sib && (sib->right && sib->left) && sib->left->color == BLACK && sib->right->color == BLACK)) {
@@ -242,7 +286,7 @@ public:
 				fixDB(node->parent);
 			} else if ((node->parent->left == node && sib->left && sib->left->color == RED) 
 					|| (node->parent->right == node && sib->right && sib->right->color == RED)) {
-					Node* nc;
+				Node* nc;
 				if (node->parent->left == node && sib->left && sib->left->color == RED) {
 					nc = sib->left;
 					tmp = LRotation(RRotation(sib)->parent);
@@ -300,7 +344,159 @@ public:
 		}
 	}
 	~RedBlackTree() {};
+	void _getTotalLvls( Node *x, const int lvl ) {
+		if ( !x ) return;
+		else if ( lvl >= output::total_lvls )
+			output::total_lvls = lvl + 1;
+		_getTotalLvls( x->left, lvl + 1 );
+		_getTotalLvls( x->right, lvl + 1 );
+	};
+	void _store( Node *x, std::vector< output > &out, const int lvl, e_directions dir ) {
+		if ( lvl == output::total_lvls ) return;
+		_store( ( x ? x->left : x ), out, lvl + 1, LEFT );
+		std::stringstream ss;
+		if ( x ) ss << x->data;
+		out.push_back( output( x, lvl, ss.str().length(), dir ) );
+		_store( ( x ? x->right : x ), out, lvl + 1, RIGHT );
+	};
+	void _log( std::vector< output > out ) {
+		if ( !out.size() ) {
+			std::cout << "empty rbt\n";
+			return;
+		}
+		int indent = 2;
+		for ( int l = output::total_lvls - 1, i = -1, lsl = -1, diff = 2; l >= 0;
+			  l--, i = -1, lsl = -1, diff *= 2 ) {
+			while ( ++i < out.size() ) {
+				if ( l != out[ i ]._lvl ) continue;
+				if ( lsl == -1 && l == output::total_lvls - 1 ) out[ i ]._spaces = 0;
+				else
+					out[ i ]._spaces = indent + out[ i ]._addit;
+				out[ i ]._addit += out[ i ]._length;
+				if ( out[ i ]._dir == RIGHT && out[ i - ( diff / 2 ) ]._dir == RIGHT )
+					out[ i - ( diff / 2 ) ]._addit +=
+						out[ i ]._addit + ( out[ i ]._length + out[ i - diff ]._length ) / 2;
+				if ( lsl == -1 ) indent *= ( indent ? 2 : 1 );
+				lsl = i;
+			}
+			output::total_diff = diff;
+		}
+		// std::cout << std::setw( 5 ) << "@|" << std::setw( 11 ) << "nb|" << std::setw( 7 ) <<
+		// "lvl|"
+		// 		  << std::setw( 7 ) << "sp|" << std::setw( 7 ) << "addit|" << '\n';
+		// for ( int i = 0; i < out.size(); i++ )
+		// 	std::cout << std::setw( 4 ) << i << "|" << std::setw( 10 )
+		// 			  << ( out[ i ]._nd ? out[ i ]._nd->_elt : 0 ) << "|" << std::setw( 6 )
+		// 			  << out[ i ]._lvl << "|" << std::setw( 6 ) << out[ i ]._spaces << "|"
+		// 			  << std::setw( 6 ) << out[ i ]._addit << "|" << '\n';
+		for ( int l = 0, i = -1, diff = output::total_diff, width = 0; l < output::total_lvls;
+			  l++, i = -1, diff /= 2, width = 0 ) {
+			if ( l != 0 ) {
+				std::cout << "\e[2m";
+				while ( ++i < out.size() ) {
+					if ( l != out[ i ]._lvl ) continue;
+					for ( int s = 0, end = out[ i ]._spaces + out[ i ]._length / 2; s < end; s++ ) {
+						if ( out[ i ]._dir == RIGHT && out[ i ]._nd && out[ i - diff ]._nd )
+							std::cout << "─";
+						else if ( out[ i ]._dir == RIGHT && out[ i ]._nd && !out[ i - diff ]._nd &&
+								  s > end / 2 )
+							std::cout << ( s == end / 2 + 1 ? "└" : "─" );
+						else if ( out[ i ]._dir == RIGHT && !out[ i ]._nd && out[ i - diff ]._nd &&
+								  s < end / 2 )
+							std::cout << ( s == end / 2 - 1 ? "┘" : "─" );
+						else
+							std::cout << " ";
+					}
+					if ( out[ i ]._nd ) std::cout << ( out[ i ]._dir == LEFT ? "┌" : "┐" );
+					else
+						std::cout << ' ';
+				}
+				std::cout << "\e[22m\n";
+			}
+			i = -1;
+			while ( ++i < out.size() ) {
+				if ( l != out[ i ]._lvl ) continue;
+				for ( int s = 0; s < out[ i ]._spaces; s++, width++ ) std::cout << ' ';
+				if ( out[ i ]._nd )
+					std::cout << ( out[ i ]._nd->color == RED ? "\e[1;31m" : "\e[1;37m" )
+							  << ( out[ i ]._nd->color == DBLACK ? "\e[4m" : "" )
+							  << out[ i ]._nd->data << "\e[0m";
+				else
+					std::cout << ' ';
+				width += out[ i ]._length;
+			}
+			output::_width = std::max( width + 10, output::_width );
+			std::cout << '\n';
+		}
+		std::cout << "\e[2m";
+		for ( int i = 0; i < output::_width; i++ ) std::cout << "─";
+		std::cout << "\e[22m\n";
+	};
+	// tmp
+	void log( void ) {
+		log(root);
+	}
+	void log( Node* nd ) {
+		if ( !nd ) return;
+		output::total_lvls = 0;
+		output::total_diff = 2;
+		output::_width	   = 0;
+		std::vector< output > out;
+		_getTotalLvls( nd, 0 );
+		_store( nd, out, 0, DEFAULT );
+		_log( out );
+	};
+	// checker {made by: jalalium}
+	bool check( Node *nd, std::map< Node *, std::vector< int > > &mp ) {
+		bool ok = true;
+		if ( nd->left != NULL ) {
+			if ( nd->data <= nd->left->data ) {
+				std::cout << "LEFT CHILD LARGER OR EQUAL TO PARENT " << nd->data << " "
+						  << nd->left->data << std::endl;
+				return false;
+			}
+			if ( nd->color == RED && nd->left->color == RED ) {
+				std::cout << "RED PARENT WITH RED CHILD: " << nd->data << " " << nd->left->data
+						  << std::endl;
+				return false;
+			}
+			ok &= check( nd->left, mp );
+		}
+		if ( nd->right != NULL ) {
+			if ( nd->data >= nd->right->data ) {
+				std::cout << "RIGHT CHILD SMALLER OR EQUAL TO PARENT " << nd->data << " "
+						  << nd->left->data << std::endl;
+				return false;
+			}
+			if ( nd->color == RED && nd->right->color == RED ) {
+				std::cout << "RED PARENT WITH RED CHILD: " << nd->data << " " << nd->right->data
+						  << std::endl;
+				return false;
+			}
+			ok &= check( nd->right, mp );
+		}
+		std::vector< int > tmp;
+		if ( nd->left != NULL ) {
+			for ( int z : mp[ nd->left ] ) tmp.push_back( z );
+		} else
+			tmp.push_back( 0 );
+		if ( nd->right != NULL ) {
+			for ( int z : mp[ nd->right ] ) tmp.push_back( z );
+		} else
+			tmp.push_back( 0 );
+		std::sort( tmp.begin(), tmp.end() );
+		for ( int &z : tmp ) z += ( nd->color == BLACK );
+		mp[ nd ] = tmp;
+		if ( tmp[ 0 ] != tmp.back() ) {
+			std::cout << "NODE " << nd->data << std::endl;
+			for ( int z : tmp ) std::cout << z << " ";
+			std::cout << std::endl;
+		}
+		return ( ok & ( tmp[ 0 ] == tmp.back() ) );
+	}
+	// end of checker!
 };
+
 
 void inorder(Node* root)
 {
@@ -310,6 +506,42 @@ void inorder(Node* root)
 		std::cout << root->data << " ";
 		inorder(root->right);
 	}
+}
+
+bool isBalancedUtil(Node *root, int &maxh, int &minh)
+{
+    // Base case
+    if (root == NULL)
+    {
+        maxh = minh = 0;
+        return true;
+    }
+ 
+    int lmxh, lmnh; // To store max and min heights of left subtree
+    int rmxh, rmnh; // To store max and min heights of right subtree
+ 
+    // Check if left subtree is balanced, also set lmxh and lmnh
+    if (isBalancedUtil(root->left, lmxh, lmnh) == false)
+        return false;
+ 
+    // Check if right subtree is balanced, also set rmxh and rmnh
+    if (isBalancedUtil(root->right, rmxh, rmnh) == false)
+        return false;
+ 
+    // Set the max and min heights of this node for the parent call
+    maxh = std::max(lmxh, rmxh) + 1;
+    minh = std::min(lmnh, rmnh) + 1;
+ 
+    // See if this node is balanced
+    if (maxh <= 2*minh)
+        return true;
+ 
+    return false;
+}
+bool isBalanced(Node *root)
+{
+    int maxh, minh;
+    return isBalancedUtil(root, maxh, minh);
 }
 
 void printGivenLevel(Node* root, int level)
@@ -324,36 +556,110 @@ void printGivenLevel(Node* root, int level)
     }
 }
 
-int main() {
+int main(void) {
+	
+	// // random
+	// {
+	// std::system( "clear" );
+	// std::cout << std::boolalpha;
+	// srand( time( NULL ) );
+	// std::random_device rd;
+	// std::mt19937	   mt( rd() );
+	// 	for ( int test = 0, total_tests = 4, sz = 100000; test < total_tests; test++ ) {
+	// 		std::cout << "\e[s\e[1;37m"
+	// 				  << "────────────────────────>"
+	// 				  << "     TEST " << test + 1 << " of " << total_tests
+	// 				  << " START    "
+	// 					 "<────────────────────────"
+	// 				  << "\e[0m\n";
+	// 		std::vector< int > in;
+	// 		for ( int i = 0, range = INT_MAX / sz; i < sz; i++ )
+	// 		// for ( int i = 0, range = 10; i < sz; i++ )
+	// 			in.push_back( rand() % range + range * i );
+	// 		std::vector< int > out( in );
+	// 		std::shuffle( in.begin(), in.end(), mt );
+	// 		std::shuffle( out.begin(), out.end(), mt );
+	// 		// std::cout << "insert ";
+	// 		// for ( int i = 0; i < sz; i++ ) std::cout << in.at( i ) << ' ';
+	// 		// std::cout << '\n';
+	// 		// std::cout << "delete ";
+	// 		// for ( int i = 0; i < sz; i++ ) std::cout << out.at( i ) << ' ';
+	// 		// std::cout << '\n';
+	// 		// exit( 0 );
+	// 		RedBlackTree n;
+	// 		std::cout << "\e[1;34m"
+	// 				  << "────────>    STARTING INSERTION    "
+	// 				  << "\e[0m\n";
+	// 		sleep( 1 );
+	// 		for ( int i = 0; i < sz; i++ ) n.insert(n.root, in.at( i ) );
+	// 		std::cout << "\e[F\e[J\e[1;32m"
+	// 				  << "────────>    INSERTION SUCCESS    "
+	// 				  << "\e[0m\n";
+	// 		sleep( 1 );
+	// 		// n.log();
+	// 		// std::cout << "\e[1;34m"
+	// 		// 		  << "────────>    STARTING DELETION    "
+	// 		// 		  << "\e[0m\n";
+	// 		// sleep( 1 );
+	// 		// for ( int i = 0; i < sz; i++ ) n.del( out.at( i ));
+	// 		// std::cout << "\e[F\e[J\e[1;32m"
+	// 		// 		  << "────────>    DELETION SUCCESS    "
+	// 		// 		  << "\e[0m\n";
+	// 		// sleep( 1 );
+	// 		std::cout << "\e[u\e[J\e[0;32m"
+	// 				  << "────────────────────────>"
+	// 				  << "    TEST " << test + 1 << " of " << total_tests
+	// 				  << " SUCCESS   "
+	// 					 "<────────────────────────"
+	// 				  << "\e[0m\n";
+	// 		// n.log();
+	// 	}
+	// 	std::cout << "\e[1;32m"
+	// 			  << "────────────────────────>    SUCCEEDED ALL TESTS   "
+	// 				 "<────────────────────────"
+	// 			  << "\e[0m\n";
+	// }
+	
+	// {
 	RedBlackTree obj;
 
-	obj.insert(obj.root, 50);
-	obj.insert(obj.root, 60);
-	obj.insert(obj.root, 20);
-	// print2D(obj.root);
-	obj.insert(obj.root, 10);
-	// print2D(obj.root);
-	obj.insert(obj.root, 15);
-	// print2D(obj.root);
-	obj.insert(obj.root, 18);
-	// print2D(obj.root);
-	obj.insert(obj.root, 16);
-	print2D(obj.root);
+	obj.insert(obj.root, 1);
+	obj.insert(obj.root, 0);
+	obj.insert(obj.root, 2);
+	// obj.log();
+	obj.insert(obj.root, 3);
+	// obj.log();
+	obj.insert(obj.root, 4);
+	// obj.log();
+	obj.insert(obj.root, 5);
+	// obj.log();
+	obj.insert(obj.root, 6);
+	// std::cout << "right after insert 6\n";
+	// obj.log();
+	// obj.insert(obj.root, 7);
+	// for (int i = 0; i < 8; i++)
+	// {
+	// 	obj.insert(obj.root, i);
+	// 	if (!isBalanced(obj.root))
+	// 	{
+	// 		std::cout << "shot " << std::endl;
+	// 		exit(5);
+	// 	}
+	// }
+	// std::cout << "right after insert 7\n";
+	obj.log();
 	// Node* tst = obj.SearchNode(obj.root, 16);
 	// if (tst)
 	// 	std::cout << tst->data <<"\n";
 	// else
 	// 	std::cout << "no";
-	std::cout << "--> delete\n";
-	obj.deleteNode(obj.root, 20);
-	print2D(obj.root);
-	obj.deleteNode(obj.root, 10);
-	print2D(obj.root);
-	obj.deleteNode(obj.root, 60);
-	print2D(obj.root);
-	// obj.root = obj.deleteNode(obj.root, 16);
-	// print2D(obj.root);
-	// obj.root = obj.deleteNode(obj.root, 16);
-	// print2D(obj.root);
+	// std::cout << "--> delete\n";
+	obj.deleteNode(obj.root, 0);
+	obj.log();
+	// obj.deleteNode(obj.root, 10);
+	// obj.log();
+	// obj.deleteNode(obj.root, 60);
+	// obj.log();
+	// }
 	return 0;
 }
