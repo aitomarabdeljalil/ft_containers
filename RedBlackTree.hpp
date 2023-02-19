a1d0cd862885103a88977aee90a5c1ca5cd4fe9c
Abdeljalil Ait omar <aait-oma@e2r2p12.1337.ma>
2023-02-19 19:00:09 +0100

    satrting vector

--- RedBlackTree.hpp
+++ RedBlackTree.hpp
@@ -6,7 +6,7 @@
 /*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 14:20:36 by aait-oma          #+#    #+#             */
-/*   Updated: 2023/02/18 18:55:49 by aait-oma         ###   ########.fr       */
+/*   Updated: 2023/02/19 11:48:06 by aait-oma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,6 +49,9 @@ struct Node
 		this->left = this->right = NULL;
 		event = def;
 	}
+	bool		isRed() {
+		return (this->color == RED) : true ? false;
+	}
 	Node<T>*    getParent() {
 		return this->parent;
 	}
@@ -56,14 +59,6 @@ struct Node
 		if (this->left || this->right)
 			return true;
 		return false;
-	}
-	Node<T>*	getUncle() {
-		Node<T>*	gParent = this->parent ? this->parent->parent : NULL;
-		if (gParent && this->data < gParent->data)
-			return gParent->right;
-		else if (gParent && this->data > gParent->data)
-			return gParent->left;
-		return NULL;
 	}
 	Node<T>*    findSib() {
 		Node<T>*    _parent = this->getParent();
@@ -76,218 +71,218 @@ struct Node
 template <class T>
 class RedBlackTree
 {
-private:
-	/*--- Rotations ---*/
-	Node<T>*   RRotation(Node<T>* node)
-	{
-		Node<T>* p = node->left;
-		Node<T>* b = p->right;
-		if (node == root) root = p;
-		if ( b ) b->parent = node;
-		node->left = b;
-		p->parent = node->parent;
-		Node<T>* qparent = node->parent;
-		if (qparent) {
-			if (qparent->data > node->data)
-				qparent->left = p;
-			else
-				qparent->right = p;
-		}    
-		p->right = node;
-		node->parent = p;
-		return p;
-	}
-	Node<T>*   LRotation(Node<T>* node)
-	{
-		Node<T>* q = node->right;
-		Node<T>* b = q->left;
-		if (node == root) root = q;
-		node->right = b;
-		if (b) b->parent = node;
-		q->left = node;
-		q->parent = node->parent;
-		if (node->parent) {
-			if (node->parent->data > node->data)
-				node->parent->left = q;
-			else
-				node->parent->right = q;
+	private:
+		/*--- Rotations ---*/
+		Node<T>*   RRotation(Node<T>* node)
+		{
+			Node<T>* p = node->left;
+			Node<T>* b = p->right;
+			if (node == root) root = p;
+			if ( b ) b->parent = node;
+			node->left = b;
+			p->parent = node->parent;
+			Node<T>* qparent = node->parent;
+			if (qparent) {
+				if (qparent->data > node->data)
+					qparent->left = p;
+				else
+					qparent->right = p;
+			}    
+			p->right = node;
+			node->parent = p;
+			return p;
 		}
-		node->parent = q;
-		return q;
-	}
-	/*--- recolore ---*/
-	void	colorSwap(Node* nd1, Node* nd2)
-	{
-		Color colorTmp = nd1->color;
-		nd1->color = nd2->color;
-		nd2->color = colorTmp;
-	}
-	/*--- insertion ---*/
-	Node<T>*	rbInsertFixup(Node<T>* node, int key, Node<T>* _parent = NULL)
-	{
-		if (!node) {
-			node = new Node(key, _parent);
+		Node<T>*   LRotation(Node<T>* node)
+		{
+			Node<T>* q = node->right;
+			Node<T>* b = q->left;
+			if (node == root) root = q;
+			node->right = b;
+			if (b) b->parent = node;
+			q->left = node;
+			q->parent = node->parent;
+			if (node->parent) {
+				if (node->parent->data > node->data)
+					node->parent->left = q;
+				else
+					node->parent->right = q;
+			}
+			node->parent = q;
+			return q;
+		}
+		/*--- recolore ---*/
+		void	colorSwap(Node* nd1, Node* nd2)
+		{
+			Color colorTmp = nd1->color;
+			nd1->color = nd2->color;
+			nd2->color = colorTmp;
+		}
+		/*--- insertion ---*/
+		Node<T>*	rbInsertFixup(Node<T>* node, T key, Node<T>* _parent = NULL)
+		{
+			if (!node) {
+				node = new Node(key, _parent);
+				return node;
+			}
+			else if (key > node->data)
+				node->right = rbInsertFixup(node->right, key, node);
+			else if (key < node->data)
+				node->left = rbInsertFixup(node->left, key, node);
+			else
+				return node;
+			if (!node->getParent())
+				node->color = BLACK;
+			else if ((node->left && node->left->color == RED) ||  (node->right && node->right->color == RED)) {
+				if (node->color == RED) {
+					Node<T>* sib = node->findSib();
+					if (sib && sib->color == RED) {
+						node->color = BLACK;
+						sib->color = BLACK;
+						node->getParent()->color = RED;
+					} 
+					else {
+						if (key < node->data && node->data < node->getParent()->data) {
+							node->getParent()->event = RightRotColorSwap;
+						} else if (key > node->data && node->data > node->getParent()->data) {
+							node->getParent()->event = LeftRotColorSwap;
+						} else if (key > node->data &&  node->data < node->getParent()->data) {
+							node->event = LeftRot;
+							node->getParent()->event = RightRotColorSwap;
+						} else {
+							node->event = RightRot;
+							node->getParent()->event = LeftRotColorSwap;
+						}
+					}
+				}	
+			}
+			if (node->event != def) {
+				Events ev = node->event;
+				node->event = def;
+				if (ev == RightRot || ev == RightRotColorSwap) {
+					node = RRotation(node);
+					if (ev == RightRotColorSwap)
+						colorSwap(node, node->right);
+				}
+				if (ev == LeftRot || ev == LeftRotColorSwap) {
+					node = LRotation(node);
+					if (ev == LeftRotColorSwap)
+						colorSwap(node, node->left);
+				}
+			}
 			return node;
 		}
-		else if (key > node->data)
-			node->right = rbInsertFixup(node->right, key, node);
-		else if (key < node->data)
-			node->left = rbInsertFixup(node->left, key, node);
-		else
-			return node;
-		if (!node->parent)
-			node->color = BLACK;
-		else if ((node->left && node->left->color == RED) ||  (node->right && node->right->color == RED)) {
-			if (node->color == RED) {
-				Node<T>* sib = node->findSib();
-				if (sib && sib->color == RED) {
+		/*--- deletion ---*/
+		void	fixDB(Node<T>* node)
+		{
+			Node<T>* tmp = node;
+			if (node->color != DBLACK) return ;
+			if (!node->getParent()) {
+				node->color = BLACK;
+				return ;
+			}
+			Node<T>* sib = node->findSib();
+			Node<T>* nparent = node->getParent();
+			Node<T>* grandparent = sib->getParent()->parent;
+			if (sib && sib->isRed()) {
+				if (nparent->left == node)
+					nparent = LRotation(nparent);
+				else
+					nparent = RRotation(nparent);
+				colorSwap(nparent, sib);
+				fixDB(node);
+			} else {
+				if (!sib || (sib && !sib->hasChild())
+					|| (sib && (sib->right && sib->left) && sib->left->color == BLACK && sib->right->color == BLACK)) {
 					node->color = BLACK;
-					sib->color = BLACK;
-					node->parent->color = RED;
-				} 
-				else {
-					if (key < node->data && node->data < node->parent->data) {
-						node->parent->event = RightRotColorSwap;
-					} else if (key > node->data && node->data > node->parent->data) {
-						node->parent->event = LeftRotColorSwap;
-					} else if (key > node->data &&  node->data < node->parent->data) {
-						node->event = LeftRot;
-						node->parent->event = RightRotColorSwap;
+					node->parent->color = Color(node->parent->color + 1);
+					if (sib) sib->color = RED;
+					fixDB(node->parent);
+				} else if ((node->parent->left == node && sib->left && sib->left->color == RED) 
+						|| (node->parent->right == node && sib->right && sib->right->color == RED)) {
+					Node<T>* nc;
+					node->color = BLACK;
+					if (node->parent->left == node && sib->left && sib->left->color == RED) {
+						nc = sib->left;
+						tmp = LRotation(RRotation(sib)->parent);
+						colorSwap(tmp, node->parent);
+						tmp->left->color=BLACK;
 					} else {
-						node->event = RightRot;
-						node->parent->event = LeftRotColorSwap;
+						nc = sib->right;
+						tmp = RRotation(LRotation(sib)->parent);
+						colorSwap(tmp, node->parent);
+						tmp->right->color=BLACK;
+					}
+				} else {
+					Node<T>* np = NULL;
+					node->color = BLACK;
+					if (node->parent->left == node && sib->right && sib->right->color == RED) {
+						np = sib->right;
+						tmp = LRotation(sib->parent);
+						colorSwap(tmp, node->parent);
+						tmp->left->color=BLACK;
+						tmp->right->color=BLACK;
+					} else {
+						np = sib->left;
+						tmp = RRotation(sib->parent);
+						colorSwap(tmp, node->parent);
+						tmp->left->color=BLACK;
+						tmp->right->color=BLACK;
 					}
 				}
-			}	
-		}
-		if (node->event != def) {
-			Events ev = node->event;
-			node->event = def;
-			if (ev == RightRot || ev == RightRotColorSwap) {
-				node = RRotation(node);
-				if (ev == RightRotColorSwap)
-					colorSwap(node, node->right);
-			}
-			if (ev == LeftRot || ev == LeftRotColorSwap) {
-				node = LRotation(node);
-				if (ev == LeftRotColorSwap)
-					colorSwap(node, node->left);
 			}
 		}
-		return node;
-	}
-	/*--- deletion ---*/
-	void	fixDB(Node<T>* node)
-	{
-		Node<T>* tmp = node;
-		if (node->color != DBLACK) return ;
-		if (!node->parent) {
-			node->color = BLACK;
-			return ;
-		}
-		Node<T>* sib = node->findSib();
-		Node<T>* nparent = node->getParent();
-		Node<T>* grandparent = sib->parent->parent;
-		if (sib && sib->color == RED) {
-			if (nparent->left == node)
-				nparent = LRotation(nparent);
-			else
-				nparent = RRotation(nparent);
-			colorSwap(nparent, sib);
-			fixDB(node);
-		} else {
-			if (!sib || (sib && !sib->hasChild())
-				|| (sib && (sib->right && sib->left) && sib->left->color == BLACK && sib->right->color == BLACK)) {
-				node->color = BLACK;
-				node->parent->color = Color(node->parent->color + 1);
-				if (sib) sib->color = RED;
-				fixDB(node->parent);
-			} else if ((node->parent->left == node && sib->left && sib->left->color == RED) 
-					|| (node->parent->right == node && sib->right && sib->right->color == RED)) {
-				Node<T>* nc;
-				node->color = BLACK;
-				if (node->parent->left == node && sib->left && sib->left->color == RED) {
-					nc = sib->left;
-					tmp = LRotation(RRotation(sib)->parent);
-					colorSwap(tmp, node->parent);
-					tmp->left->color=BLACK;
-				} else {
-					nc = sib->right;
-					tmp = RRotation(LRotation(sib)->parent);
-					colorSwap(tmp, node->parent);
-					tmp->right->color=BLACK;
-				}
+		void	deleteNode(Node<T> *&node, T key)
+		{
+			//BST deletion
+			Node<T>	*tmp;
+			if (!node)
+				return ;
+			else if (key < node->data)
+				deleteNode(node->left, key);
+			else if (key > node->data)
+				deleteNode(node->right, key);
+			else if (!node->left && !node->right) {
+				if (!node->isRed())
+					node->color = DBLACK;
+				fixDB(node);
+				delete(node);
+				node = NULL;
+			} else if (!node->left || !node->right) {
+				tmp = node;
+				if (!node->left)
+					node = node->right;
+				else if (!node->right)
+					node = node->left;
+				node->parent = tmp->parent;
+				node->color = tmp->color;
+				delete tmp;
+				tmp = NULL;
 			} else {
-				Node<T>* np = NULL;
-				node->color = BLACK;
-				if (node->parent->left == node && sib->right && sib->right->color == RED) {
-					np = sib->right;
-					tmp = LRotation(sib->parent);
-					colorSwap(tmp, node->parent);
-					tmp->left->color=BLACK;
-					tmp->right->color=BLACK;
-				} else {
-					np = sib->left;
-					tmp = RRotation(sib->parent);
-					colorSwap(tmp, node->parent);
-					tmp->left->color=BLACK;
-					tmp->right->color=BLACK;
-				}
+				tmp = findMin(node->right);
+				node->data = tmp->data;
+				deleteNode(node->right, tmp->data);
 			}
 		}
-	}
-	void	deleteNode(Node<T> *&node, T key)
-	{
-		//BST deletion
-		Node<T>	*tmp;
-		if (!node)
-			return ;
-		else if (key < node->data)
-			deleteNode(node->left, key);
-		else if (key > node->data)
-			deleteNode(node->right, key);
-		else if (!node->left && !node->right){
-			if (node->color == BLACK)
-				node->color = DBLACK;
-			fixDB(node);
-			delete(node);
-			node = NULL;
-		} else if (!node->left || !node->right){
-			tmp = node;
-			if (!node->left)
-				node = node->right;
-			else if (!node->right)
-				node = node->left;
-			node->parent = tmp->parent;
-			node->color = tmp->color;
-			delete tmp;
-			tmp = NULL;
-		} else {
-			tmp = findMin(node->right);
-			node->data = tmp->data;
-			deleteNode(node->right, tmp->data);
+	public:
+		RedBlackTree();
+		void		insert(T key) {
+			root = rbInsertFixup(root, key);
 		}
-	}
-public:
-	RedBlackTree();
-	void		insert(T key) {
-		root = rbInsertFixup(root, key);
-	}
-	Node<T>*	find(T key) {
-		Node<T>*	node = root;
+		Node<T>*	find(T key) {
+			Node<T>*	node = root;
 
-		if (!node || node->data == key)
-			return node;
-		if (key < node->data)
-			return find(node->left, key);
-		return find(node->right, key);
-	}
-	void		remove(T key) {
-		deleteNode(root, key);
-	}
-	~RedBlackTree();
-private:
-	Node<T>* root;
+			if (!node || node->data == key)
+				return node;
+			if (key < node->data)
+				return find(node->left, key);
+			return find(node->right, key);
+		}
+		void		remove(T key) {
+			deleteNode(root, key);
+		}
+		~RedBlackTree();
+	private:
+		Node<T>* root;
 };
 
 #endif
--- containers/vector.hpp
+++ containers/vector.hpp
@@ -6,8 +6,74 @@
 /*   By: aait-oma <aait-oma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 17:01:32 by aait-oma          #+#    #+#             */
-/*   Updated: 2023/02/18 11:49:33 by aait-oma         ###   ########.fr       */
+/*   Updated: 2023/02/19 17:56:22 by aait-oma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+# include <iostream>
+
+namespace ft {
+	template < class T, class Alloc = std::allocator<T> >
+	class vector
+	{
+		public:
+			vector() {
+				
+			};
+			bool empty() const {}
+			~vector(){};
+			typedef T 											value_type;
+			typedef Alloc										allocator_type;
+			typedef typename allocator_type::size_type 			size_type;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+			bool empty() const { return this->_size == 0; };
+			size_type size() const { return this->_size; };
+			reference front(){
+				return this->_array[0];
+			};
+			const_reference front() const{
+				return this->_array[0];
+			};
+			reference back(){
+				return this->_array[this->_size - 1];
+			};
+			const_reference back() const{
+				return this->_array[this->_size - 1];
+			};
+			reference operator[] (size_type n){
+				return this->_array[n];
+			};
+			const_reference operator[] (size_type n) const{
+				return this->_array[n];
+			};
+			reference at (size_type n){
+				if (n >= this->_size)
+					throw std::out_of_range("vector");
+				return this->_array[n];
+			};
+			const_reference at (size_type n) const{
+				if (n >= this->_size)
+					throw std::out_of_range("vector");
+				return this->_array[n];
+			};
+			// size_type max_size() const{
+			// 	return std::numeric_limits<difference_type>::max();
+			// }
+			allocator_type get_allocator() const {
+				return this->_alloc;
+			};
+
+		private:
+			T*				_array;
+			allocator_type	_alloc;
+			size_type		_size;
+			size_type		_capacity;
+	};
+}
+#endif
